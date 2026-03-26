906d5aae5efa54e5e0eb8b05c809295ad150baf7
djole994 <djordjeradovic94@gmail.com>
2026-03-26 10:04:48 +0100

    product-2

---
+++ TechStockSync.Api/DTOs/ProductDto.cs
@@ -0,0 +1,11 @@
+﻿namespace TechStockSync.Api.DTOs
+{
+    public class ProductDto
+    {
+        public string Sku { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+        public string CategoryName { get; set; } = string.Empty; 
+        public decimal Price { get; set; }
+        public int StockQuantity { get; set; }
+    }
+}
